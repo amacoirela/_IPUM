4e0ab7b3bb5affbb492b914e9f2b40c30cac2417
Monika <monika.fafara@outlook.com>
2025-04-13 14:07:24 +0200

    message

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ipum!")
+import argparse
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None: ...  # implement me!
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
